ab9de776612742bb59a08ebceb84ebdfde705652
Ankit Saini <ankit.saini@metacube.com>
2021-03-15 11:53:48 +0530

    updated sort

--- Assignment2_DSA/SortEmployeeLinkedList.java
+++ Assignment2_DSA/SortEmployeeLinkedList.java
@@ -46,6 +46,7 @@ public class SortEmployeeLinkedList {
 	
 	
 	public void sortEmployeeLinkedList(){
+	    if (head == null || head.next == null) return ;
 		EmployeeNode currNode = head ,sortedHead = null ;
 		while(currNode != null){
 			EmployeeNode currNext = currNode.next;
@@ -57,33 +58,37 @@ public class SortEmployeeLinkedList {
 	
 	public EmployeeNode sort(EmployeeNode sortedHead, EmployeeNode currNode){
 		
-		if(sortedHead == null || sortedHead.salary < currNode.salary){
+		if(sortedHead == null || ( (sortedHead.salary < currNode.salary) || (sortedHead.salary.equals(currNode.salary) && (sortedHead.age > currNode.age) ) )){
 			currNode.next = sortedHead;
 			sortedHead = currNode;
 		}
 		else{
 			EmployeeNode predNode = sortedHead;
-			while(predNode.next != null && predNode.next.salary >= currNode.salary && predNode.next.age < currNode.age ){
-				predNode = predNode.next;
+			
+			while(predNode.next != null &&((predNode.next.salary > currNode.salary) ||((predNode.next.salary.equals(currNode.salary)) && (predNode.next.age < currNode.age)) )  ){
+					predNode = predNode.next;
 			}
 			currNode.next = predNode.next;
 			predNode.next = currNode;
+		
 		}
 		return sortedHead;
 	}
-	
+
 	public static void main(String[] args){
 		SortEmployeeLinkedList list = new SortEmployeeLinkedList();
 		list.insert("A", 10000. ,20);
 		list.insert("B", 12000. , 21);
 		list.insert("C", 7000. ,22);
+		list.insert("P", 8000., 19);
 		list.insert("D", 8000. , 23);
 		list.insert("E", 8000. , 24);
 		list.insert("F", 8000. ,25);
-		list.insert("G", 6000. ,26);
+		list.insert("G", 6000. ,21);
 		list.insert("H", 13000. ,27);
 		list.insert("Y", 8000. , 43);
 		list.insert("Z", 8000. , 24);
+		list.insert("P", 8000., 17);
 		list.printEmployeeLinkList();
 		list.sortEmployeeLinkedList();
 		System.out.println("After Sorting");
